d175b9eb99f0955bcfec8e2a75e60bb53a4366cf
Alexey Vasilyev <vampitest@yandex.ru>
2024-08-04 11:42:30 +0300

    Reorganized design and button interaction

--- window.py
+++ window.py
@@ -6,17 +6,21 @@ from file_manager import FileManager
 from PyQt6.QtCore import QSize
 from PyQt6.QtWidgets import QApplication, QPushButton, QFileDialog, QWidget, QPlainTextEdit
 
-WINDOW_HEIGHT = 600
-WINDOW_WIDTH  = 800
+BLOCK_SIZE = 60
 
-BUTTON_HEIGHT = 60
-BUTTON_WIDTH  = 120
+WINDOW_HEIGHT = 8*BLOCK_SIZE
+WINDOW_WIDTH  = 12*BLOCK_SIZE
 
+scale_on_grid = lambda sizes: [BLOCK_SIZE*sizes[0], BLOCK_SIZE*sizes[1]]
+shift_on_grid = lambda pos: [BLOCK_SIZE*pos[0] + BLOCK_SIZE//2, BLOCK_SIZE*pos[1] + BLOCK_SIZE//2]
+
+# shift_on_grid = lambda pos: [BLOCK_SIZE*pos[0] + BLOCK_SIZE//2, BLOCK_SIZE*pos[1] + BLOCK_SIZE//2]
+    
 class Stage(IntEnum):
     START = 0,
-    CRYPT  = 1,
+    MODE  = 1,
     FILES = 2,
-    RUN   = 3,
+    CRYPT   = 3,
 
 class Window(QWidget):
     def __init__(self):
@@ -27,80 +31,129 @@ class Window(QWidget):
         self.setWindowTitle("Encryptor")
         self.setFixedSize(QSize(WINDOW_WIDTH, WINDOW_HEIGHT))
         
-        self._init_stage_buttons()
+        # self._init_stage_buttons()
         self.reset_btn = self._add_button(
             "RESET",
             self.reset,
-            [BUTTON_HEIGHT, BUTTON_HEIGHT],
-            [WINDOW_WIDTH - BUTTON_HEIGHT, WINDOW_HEIGHT - BUTTON_HEIGHT],
-            False
+            [1, 1],
+            [0, 0],
+            False,
+            True
         )
-        self.mode_btn = self._add_button(
-            self.mode.name,
-            self.switch_mode,
-            [BUTTON_HEIGHT, BUTTON_HEIGHT],
-            [(WINDOW_WIDTH * (2 * Stage.CRYPT.value + 1) // len(Stage) - BUTTON_WIDTH) // 2, BUTTON_HEIGHT // 2],
-            False
-            )
         
-        self.text_box = QPlainTextEdit(parent=self)
-        self.text_box.setFixedSize(4 * BUTTON_WIDTH, 5 * BUTTON_HEIGHT)
-        self.text_box.move(WINDOW_WIDTH // 2 - 2 * BUTTON_WIDTH, 2 * BUTTON_HEIGHT)
-        self.text_box.setReadOnly(True)
+        self.reset_btn = self._add_button(
+            "NEXT",
+            self.next_stage,
+            [1, 1],
+            [10, 0],
+            False,
+            True
+        )
+        
+        # START stage
+        # self.start_btn = self._add_button(
+        #     Stage.START.name,
+        #     self.pick_working_dir,
+        #     [1, 1],
+        #     [5, 0],
+        # )
+        
+        self.message_box = QPlainTextEdit(parent=self)
+        ms = scale_on_grid([6, 5])
+        self.message_box.setFixedSize(ms[0], ms[1])
+        mp = shift_on_grid([0, 2])
+        self.message_box.move(mp[0], mp[1])
+        self.message_box.setReadOnly(True)
+        
+        # MODE stage        
+        self.mode_btn = self._add_button(
+            Mode.EN.name,
+            self.switch_mode,
+            [1, 1],
+            [5, 0],
+        )
+        
+        # FILES stage
+        # self.files_btn = self._add_button(
+        #     Stage.FILES.name,
+        #     self.choose_files,
+        #     [1, 1],
+        #     [5, 0],
+        # )
+        
+        # CRYPT stage
+        self.key_box = QPlainTextEdit(parent=self)
+        ks = scale_on_grid([4, 3])
+        self.key_box.setFixedSize(ks[0], ks[1])
+        kp = shift_on_grid([7, 2])
+        self.key_box.move(kp[0] ,kp[1])
         
         # layout = QVBoxLayout()
         # layout.addWidget(start_button)
         # self.setLayout(layout)
         self.reset()
 
-    def _init_stage_buttons(self):
-        self.buttons = {}
-        stage_to_method = {
-            Stage.START: self.pick_working_dir,
-            Stage.CRYPT:  self.set_mode,
-            Stage.FILES: self.choose_files,
-            Stage.RUN:   self.crypt_files
-        }
-        for stage in Stage:
-            w = BUTTON_WIDTH
-            h = BUTTON_HEIGHT
-            x = (WINDOW_WIDTH * (2 * stage.value + 1) // len(Stage) - BUTTON_WIDTH) // 2
-            y = BUTTON_HEIGHT // 2
-            if stage == Stage.CRYPT:
-                w = w // 2
-                x = x + w
-            self.buttons[stage] = self._add_button(
-                stage.name, 
-                stage_to_method[stage], 
-                [w, h],
-                [x, y],
-                True
-            )
-            self.buttons[stage].clicked.connect(self.next_stage)
+    # def _init_stage_buttons(self):
+    #     self.buttons = {}
+    #     stage_to_method = {
+    #         Stage.START: self.pick_working_dir,
+    #         Stage.MODE: self.set_mode,
+    #         Stage.FILES: self.choose_files,
+    #         Stage.CRYPT:   self.crypt_files
+        # }
+        # for stage in Stage:
+        #     w = BLOCK_SIZE
+        #     h = BLOCK_SIZE
+        #     x = (WINDOW_WIDTH * (2 * stage.value + 1) // len(Stage) - BLOCK_SIZE) // 2
+        #     y = BLOCK_SIZE // 2
+        #     if stage == Stage.MODE:
+        #         w = w // 2
+        #         x = x + w
+        #     self.buttons[stage] = self._add_button(
+        #         stage.name, 
+        #         stage_to_method[stage], 
+        #         [w, h],
+        #         [x, y],
+        #         True
+        #     )
+        #     self.buttons[stage].clicked.connect(self.next_stage)
 
-    def _add_button(self, text, method_to_connect, sizes, pos, disable):
+    def _add_button(self, text, method_to_connect, sizes, pos, disabled = True, vis = False):
         button = QPushButton(text=text, parent=self)
         button.setStyleSheet("font-size: 16px;")
+        sizes = scale_on_grid(sizes)
         button.setFixedSize(sizes[0], sizes[1])
         button.clicked.connect(method_to_connect)
+        pos = shift_on_grid(pos)
         button.move(pos[0], pos[1])
-        button.setDisabled(disable)
+        button.setDisabled(disabled)
+        button.setVisible(vis)
         return button
-    
+
     def reset(self):
-        self.buttons[self.stage].setDisabled(True)
+        # self.buttons[self.stage].setDisabled(True)
         self.stage = Stage.START
-        self.buttons[self.stage].setDisabled(False)
-        self.text_box.setPlainText("")
+        # self.buttons[self.stage].setDisabled(False)
+        self.message_box.setPlainText("")
+        self.mode_btn.setDisabled(True)
+        self.mode_btn.setVisible(False)
         
     def switch_mode(self):
         self.mode = Mode(-self.mode.value)
         self.mode_btn.setText(self.mode.name)
     
     def next_stage(self):
-        self.buttons[self.stage].setDisabled(True)
+        if self.stage == Stage.START:
+            self.pick_working_dir()
+            self.mode_btn.setDisabled(False)
+            self.mode_btn.setVisible(True)
+        elif self.stage == Stage.MODE: 
+            self.set_mode()
+        elif self.stage == Stage.FILES:
+            self.choose_files()
+        elif self.stage == Stage.CRYPT:
+            self.crypt_files()
         self.stage = Stage((self.stage.value + 1) % len(Stage))
-        self.buttons[self.stage].setDisabled(False)
     
     # source: https://learndataanalysis.org/source-code-how-to-use-qfiledialog-file-dialog-in-pyqt5/
     def pick_working_dir(self):
@@ -110,12 +163,12 @@ class Window(QWidget):
         )
         fm.set_working_dir(response)
         files_list = '\n'.join(fm.files_names)
-        self.text_box.setPlainText(files_list)
+        self.message_box.setPlainText(files_list)
         print(response)
 
     def set_mode(self):
         fm.transform_names(self.mode)
-        self.text_box.setPlainText(
+        self.message_box.setPlainText(
             '\n'.join(
                 [
                     name + ' -> ' + new_name 
