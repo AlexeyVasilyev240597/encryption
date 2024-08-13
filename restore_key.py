7ecc11223088b2f86e5a7d93de3a754dc155eb67
Alexey Vasilyev <vampirets@yandex.ru>
2024-08-13 21:42:32 +0300

    Add command line arguments to restore_key

--- restore_key.py
+++ restore_key.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-
+import sys
 
 def get_begining(full_file_name: Path, key_len) -> str:
     file     = open(full_file_name, 'rb')
@@ -11,9 +11,18 @@ def get_key(block_1: str, block_2: str) -> str:
     key = bytes(a ^ b for a, b in zip(block_1, block_2))
     return key
 
-key_len = 40
-path_to_files = Path('C:/Users/valex/Documents/fun')
-block_1 = get_begining(path_to_files / Path('3JUS8n_BP'), key_len)
-block_2 = get_begining(path_to_files / Path('smile.gif'), key_len)
-key = get_key(block_1, block_2)
-print(key)
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) == 4:
+        key_len = int(args[0])
+        path_to_files = args[1]
+        block_1 = get_begining(path_to_files / Path(args[2]), key_len)
+        block_2 = get_begining(path_to_files / Path(args[3]), key_len)
+        key = get_key(block_1, block_2)
+        print(key)
+    else:
+        print("1 arg: approximate key length")
+        print("2 arg: path to working directory")
+        print("3 arg: encrypted file name")
+        print("4 arg: original file name")
+    
